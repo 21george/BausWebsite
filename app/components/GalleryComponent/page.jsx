499445b7d4286782d9554f2c7eafaf87880283dd
21george <mimgeorge05@gmail.com>
2025-04-06 15:29:56 +0200

    updtate chang

--- app/components/GalleryComponent/page.jsx
+++ app/components/GalleryComponent/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Gallery = ({ title, description, images, moreLink }) => {
+const Gallery = ({ title, description, images}) => {
   return (
     <div className="h-full py-6 sm:py-8 lg:py-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8 container">
