5b5a0f6133651e5d503727bbea350a27e2a080ba
21george <mimgeorge05@gmail.com>
2025-04-26 14:45:26 +0200

    update points

--- app/components/HeilkundeComponents/HeilkundeGrid.js
+++ app/components/HeilkundeComponents/HeilkundeGrid.js
@@ -38,10 +38,13 @@ const HeilkundeGrid = () => {
           {data[0].Note_description}
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8">
           {[data[0].points, data[0].points_1, data[0].points_2].map(
             (point, index) => (
-              <div key={index} className="sm:p-6  ">
+              <div
+                key={index}
+                className="bg-white p-4 sm:p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300"
+              >
                 <div className="flex items-center gap-2">
                   <svg
                     className="w-5 h-5 text-green-500"
@@ -57,7 +60,7 @@ const HeilkundeGrid = () => {
                       d="M5 13l4 4L19 7"
                     />
                   </svg>
-                  <p className="text-gray-800 text-base sm:text-lg leading-relaxed">
+                  <p className="text-gray-800 text-sm sm:text-base lg:text-lg leading-relaxed">
                     {point}
                   </p>
                 </div>
@@ -71,7 +74,10 @@ const HeilkundeGrid = () => {
             data[0].points1_2,
             data[0].points1_3,
           ].map((point, index) => (
-            <div key={index} className="sm:p-6 ">
+            <div
+              key={index}
+              className="bg-white p-4 sm:p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300"
+            >
               <div className="flex items-center gap-2">
                 <svg
                   className="w-5 h-5 text-green-500"
@@ -87,14 +93,14 @@ const HeilkundeGrid = () => {
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
-                <p className="text-gray-800 text-base sm:text-lg leading-relaxed">
+                <p className="text-gray-800 text-sm sm:text-base lg:text-lg leading-relaxed">
                   {point}
                 </p>
               </div>
             </div>
           ))}
 
-          <div className="p-4 sm:p-6 shadow-sm md:col-span-2">
+          <div className="p-4 sm:p-6 shadow-sm md:col-span-2 lg:col-span-3">
             <div className="flex items-center gap-2">
               <svg
                 className="w-5 h-5 text-green-500"
@@ -110,7 +116,7 @@ const HeilkundeGrid = () => {
                   d="M5 13l4 4L19 7"
                 />
               </svg>
-              <p className="text-gray-800 text-base sm:text-lg leading-relaxed">
+              <p className="text-gray-800 text-sm sm:text-base lg:text-lg leading-relaxed">
                 {data[0].points2}
               </p>
             </div>
